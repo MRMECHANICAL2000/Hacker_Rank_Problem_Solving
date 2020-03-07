5ff5ffae321f34b69ecfde2d59a57a7a09c7e934
MOHAMMED SAFI <43216227+MRMECHANICAL2000@users.noreply.github.com>
2020-03-07 12:27:02 +0530

    Update Breaking the Records.py

--- Algoritm And Data Structure/Algorithm/Breaking the Records.py
+++ Algoritm And Data Structure/Algorithm/Breaking the Records.py
@@ -1,14 +1,14 @@
-input()
-x=sorted(list(map(int,input().split())))
-
-countt=0
-val=0
-for i in x: 
-    if (x.count(i)>countt):
-        countt=x.count(i)
-        val=i
-    while i in x:
-        x.remove(i)
-        
-
-print(val)
+x=int(input())
+a=[int(i) for i in input().split()]
+maxx=a[0]
+minn=a[0]
+hig=0
+low=0
+for i in range(x):
+    if a[i]>maxx:
+        maxx=a[i]
+        hig+=1
+    elif a[i]<minn:
+        minn=a[i]
+        low+=1
+print(hig,low)
