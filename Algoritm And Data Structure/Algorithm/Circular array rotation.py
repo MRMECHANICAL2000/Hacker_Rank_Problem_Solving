136a3869da29c88f073ac812b019295107dfa492
MOHAMMED SAFI <43216227+MRMECHANICAL2000@users.noreply.github.com>
2020-04-07 12:52:06 +0530

    Update Circular array rotation.py

--- Algoritm And Data Structure/Algorithm/Circular array rotation.py
+++ Algoritm And Data Structure/Algorithm/Circular array rotation.py
@@ -1,4 +1,21 @@
-aa,k,q=list(map(int,input().split()))
+#Python-3
+#1 using slising
+
+```
+n,k,q=list(map(int,input().split()))
 a=list(map(int,input().split()))
-a=a[-k%aa:]+a[:-k%aa]
+a=a[-k%n:]+a[:-k%n]
 [print(a[int(input())]) for i in range(q)] 
+
+```
+
+#2 using math
+
+```
+
+n,k,q=list(map(int,input().split()))
+s=list(map(int,input().split()))
+for i in range(q):
+    m=int(input())
+    print (s[m-(k%n)])
+```        
